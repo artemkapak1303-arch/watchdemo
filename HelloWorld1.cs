7598b895dfab79617aabb2f7da55f8e434d1fb4e
artemkapak1303-arch <artemkapak1303@gmail.com>
2026-05-10 11:55:00 +0300

    Add files via upload

---
+++ HelloWorld1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HelloWorld1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Helloworld");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
